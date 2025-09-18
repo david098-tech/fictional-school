bc1950d462157c22269c5f4794aa5bbcb48daf6a
david098-tech <davidkufa2010@gmail.com>
2025-09-18 06:37:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const values = document.querySelectorAll('.value');
+
+function checkValues() {
+    const triggerBottom = window.innerHeight * 0.85;
+    values.forEach(val => {
+        const top = val.getBoundingClientRect().top;
+        if(top < triggerBottom) {
+            val.style.opacity = 1;
+            val.style.transform = 'translateX(0)';
+        } else {
+            val.style.opacity = 0;
+            if(val.classList.contains('right')) val.style.transform = 'translateX(100px)';
+            else val.style.transform = 'translateX(-100px)';
+        }
+    });
+}
+
+window.addEventListener('scroll', checkValues);
+window.addEventListener('load', checkValues);
+document.addEventListener("DOMContentLoaded", () => {
+  const galleryImages = document.querySelectorAll(".gallery-img");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, { threshold: 0.3 });
+
+  galleryImages.forEach(img => observer.observe(img));
+});
+const burger = document.getElementById("burger");
+const navLinks = document.getElementById("navLinks");
+
+burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
