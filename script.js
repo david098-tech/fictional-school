ec3368ddc9b4f19aebbb6c3995e54821821fd802
david098-tech <davidkufa2010@gmail.com>
2025-09-18 18:00:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ const navLinks = document.getElementById("navLinks");
 burger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+<script>
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('navLinks');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+</script>
+
